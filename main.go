ca188c0942f32a01c4899d5b3da1bc7b3e9f2f69
nk <1349805199@qq.com>
2024-10-30 23:02:07 +0800

    first

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		// right-click your code in the editor and select the <b>Debug</b> option.
 		fmt.Println("i =", 100/i)
 	}
-	fmt.Println(2)
+	fmt.Println(3)
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
