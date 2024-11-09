ab93f015fc58efee9ce0ff6b2dc5f89ce9643e7c
nk <1349805199@qq.com>
2024-11-09 09:24:09 +0800

    2024-11-09

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 func main() {
 	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
 	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	fmt.Println("2024-11-09 09:23:35")
 
 	for i := 1; i <= 5; i++ {
 		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -19,7 +18,7 @@ func main() {
 		// right-click your code in the editor and select the <b>Debug</b> option.
 		fmt.Println("i =", 100/i)
 	}
-	fmt.Println(4)
+	fmt.Println(5)
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
